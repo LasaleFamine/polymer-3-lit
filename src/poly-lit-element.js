2d99bc2c8b8f617d5b30571f06a472ce2c6754ff
Alessio Occhipinti <LasaleFamine@users.noreply.github.com>
2017-09-11 15:57:59 +0200

    Update poly-lit-element.js

--- src/poly-lit-element.js
+++ src/poly-lit-element.js
@@ -1,11 +1,17 @@
 import {Element} from '@polymer/polymer/polymer-element';
 import {PropertyAccessors} from '@polymer/polymer/lib/mixins/property-accessors';
+import {PropertyEffects} from '@polymer/polymer/lib/mixins/property-effects';
 import {render} from 'lit-html';
 
 const withLit = Base => {
-  return class extends PropertyAccessors(Base) {
-    _propertiesChanged() {
-      render(this.render(), this);
+  return class extends PropertyEffects(PropertyAccessors(Base)) {
+    _propertiesChanged(...args) {
+      super._propertiesChanged(...args);
+      render(this.render(), this.shadowRoot || this);
+    }
+    
+    static get template() {
+      return '';
     }
   };
 };
